03dce95f353ac19e4d1441cc8894a5ae73d491e9
yesiddiaz15 <82281380+yesiddiaz15@users.noreply.github.com>
2026-07-17 13:24:13 -0500

    feat: implement type-safe navigation and detail screen placeholder (#8)

---
+++ shared/src/commonMain/kotlin/com/project/nexfor/navigation/Destinations.kt
@@ -0,0 +1,24 @@
+package com.project.nexfor.navigation
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+sealed interface Screen {
+    @Serializable
+    data object TestList : Screen
+
+    @Serializable
+    data class TestDetail(val itemId: String) : Screen
+
+    @Serializable
+    data object Login : Screen
+
+    @Serializable
+    data object Schedule : Screen
+
+    @Serializable
+    data object Bill : Screen
+
+    @Serializable
+    data object Reports : Screen
+}
